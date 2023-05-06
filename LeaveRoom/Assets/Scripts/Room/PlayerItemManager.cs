bab56b0c9fa36fcec0de2ef109a8a94ef3a006d1
hemuichi <iamunnko@gmail.com>
2023-05-06 11:36:24 +0900

    Fix scene

---
+++ LeaveRoom/Assets/Scripts/Room/PlayerItemManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerItemManager
+{
+    
+}
+
+public class PlayerItem
+{
+    public enum Type
+    {
+        DeskKey,
+    }
+    
+    Type type;
+
+    public PlayerItem(Type type)
+    {
+        this.type = type;
+    }
+}
+
+public class GettableObject : MonoBehaviour
+{
+    [SerializeField] PlayerItem.Type itemType;
+
+    public PlayerItem Get()
+    {
+        return new PlayerItem(itemType);
+    }
+}
