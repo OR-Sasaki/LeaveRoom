9385dc3a38aa3df0b816be97fd07fe2817f89d53
hemuichi <iamunnko@gmail.com>
2023-05-11 22:15:49 +0900

    Add board

---
+++ LeaveRoom/Assets/Scripts/Room/Board/BoardObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BoardObject : GimmickableObject
+{
+    [SerializeField] GameObject fusenC;
+    [SerializeField] GameObject fusenQ;
+    [SerializeField] GameObject fusenS;
+    [SerializeField] GameObject fusenT;
+
+    void Start()
+    {
+        fusenC.SetActive(false);
+        fusenQ.SetActive(false);
+        fusenS.SetActive(false);
+        fusenT.SetActive(false);
+    }
+
+    public override void OnClick(PlayerItem.Type currentSelectItem, out CameraController.Target nextTarget, out TargetableObject targetableObject,
+        UnityEvent<PlayerItem.Type> onUseEvent)
+    {
+        base.OnClick(currentSelectItem, out nextTarget, out targetableObject, onUseEvent);
+
+        var targetObject = currentSelectItem switch
+        {
+            PlayerItem.Type.FusenC => fusenC,
+            PlayerItem.Type.FusenQ => fusenQ,
+            PlayerItem.Type.FusenS => fusenS,
+            PlayerItem.Type.FusenT => fusenT,
+        };
+        targetObject.SetActive(true);
+        onUseEvent.Invoke(currentSelectItem);
+    }
+}
